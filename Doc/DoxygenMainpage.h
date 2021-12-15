01286af5399c55a39aededf9f4cedaf82035933f
Francesca Attindehou <francesca.attindehou@epitech.eu>
2021-12-15 19:22:23 +0100

    [ADD] doxygen documentation

---
+++ Doc/DoxygenMainpage.h
@@ -0,0 +1,38 @@
+/**
+    @mainpage  IndieStudio Documentation
+    @authors Fèmi Facia
+             Georges Houmbadji
+             Francesca Attindehou
+             Damia Tobossi
+             Hans Vigan
+             Sergino Gounoukperou
+
+
+    Cross-platform Project (With Linux and Windows) using cmake
+
+
+    Repository github.com/EpitechIT2020/B-YEP-400-COT-4-1-indiestudio-femi.facia
+
+
+    @section IndieStudioIntro Introduction
+    Indie studio is a cross platform game development Engine for
+    3D developpement in C++.
+
+    @section IndieStudioTech Technology used
+        Our Engine use  raylib, a very simple C library for 2D/3D graphics.
+    As of today, 3D animation for .gltf models files is badly handled.
+    We advised you to use multiple .obj files to animate your assets.
+
+    @section IndieStudioGame The bomberman
+        Bomberman is one of the most famous video games of all time.
+    With over 70 franchises, ranging from the first version on MSX, ZX Spectrum and Sharp MZ-700 in 1983
+    to the latest versions on modern consoles, over 10 million units have been sold.
+    Our gameplay reference is Neo Bomberman, released on the Neo Geo and MVS systems in 1997.
+        \image html bomberman.jpg
+
+    @section IndieStudioInstall Requirements
+        - CMake 3.17
+        - Raylib installed on your linux system
+        - Visual Studio installed on Windows
+
+*/
